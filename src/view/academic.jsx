8e40201b69790f558deec465b999719b65aac19e
Randy Ananda <88238381+RandyAnanda11@users.noreply.github.com>
2023-07-13 14:14:23 +0700

    Update academic.jsx

--- src/view/academic.jsx
+++ src/view/academic.jsx
@@ -1,282 +1,125 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react-hooks/rules-of-hooks */
-import logo_pnj from "./4.png";
-import foto9 from "./9.jpg";
-import foto10 from "./10.jpg";
-import foto11 from "./11.jpg";
-import foto12 from "./12.jpg";
-import foto13 from "./13.jpg";
-import React, { useEffect, useState } from "react";
-import classNames from "classnames";
-function academic() {
-  const texts = [
-    "Kehidupan Akademik Departemen Teknik Informatika dan Komputer PNJ",
-  ];
-  const speed = 130; // Kecepatan pengetikan dalam milidetik
-
-  const [index, setIndex] = useState(0);
-  const [text, setText] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentIndex = index % texts.length;
-      const currentText = texts[currentIndex];
-
-      if (!isDeleting) {
-        setText((prevText) => prevText + currentText.charAt(text.length));
-        if (text === currentText) {
-          setIsDeleting(true);
-          clearInterval(interval);
-          setTimeout(() => setIsDeleting(false), speed);
-        }
-      } else {
-        if (text === "") {
-          setIndex((prevIndex) => prevIndex + 1);
-          setIsDeleting(false);
-          clearInterval(interval);
-        }
-      }
-    }, speed);
-
-    return () => clearInterval(interval);
-  }, [index, text, isDeleting, speed, texts]);
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+function contact() {
   return (
-    <div className="Poppins">
-      <div className="bg2 relative bg-center bg-fixed overflow-hidden">
-        <div className=" bg-gradient-to-t from-black/100 via-black/10 to-transparent mix-blend-multiply h-full "></div>
-        <div className="font-bold absolute bottom-16 left-14 font-Poppins text-left text-4xl  box-decoration-clone bg-gradient-to-r from-blue-600 to-transparent text-white px-2 backdrop-blur-sm rounded-md pl-4 border-l-4 border-white w-[80%]">
-          <p className="bg-gradient-to-br from-white  to-yellow-300 bg-clip-text text-transparent w-[70%] p-4">
-            {text}
-            <span className={classNames("opacity-50", "animate-blink")}>|</span>
-          </p>
+    <div className="font-Poppins bg-white">
+      <div className="pnj h-[750px] w-full overflow-hidden bg-bottom bg-fixed bg-no-repeat relative max-w-10xl">
+        <div className=" bg-gradient-to-t from-black/100 via-white/10 to-transparent mix-blend-multiply h-full "></div>
+        <div className="font-bold absolute bottom-40 left-[50%] -translate-x-[50%] font-Poppins text-left text-7xl  box-decoration-clone bg-gradient-to-t from-blue-600 to-transparent text-white p-6 backdrop-blur-sm rounded-md pl-4 border-b-4 border-white flex">
+          <p className="text-white text-center mx-auto">Kontak Kami</p>
         </div>
       </div>
 
-      <div>
-        <div className="border-b-4 mt-14 border-black text-left ml-24 mb text-blue-950 text-2xl font-semibold max-w-[50%]">
-          <h1>SEPUTAR AKADEMIK TIK PNJ</h1>
-        </div>
-        <div className="flex justify-between mt-10 w-full">
-          <div className="ml-24 text-xl mr-20 w-[70%]">
-            <p className="text-blue-950 text-justify">
-              Bila anda senang dengan ilmu komputer terapan dan bagaimana ilmu
-              tersebut langsung diimplementasikan dalam dunia industri, TIK PNJ
-              adalah tempat yang cocok untuk anda. <br />
-              <br />
-              Sejak tahun 2010, kami telah menerapkan pendidikan vokasional di
-              bidang ilmu komputer dengan porsi pembelajaran praktik 60% dan
-              teori 40% sehingga Mahasiswa TIK PNJ hanya difokuskan dengan mata
-              kuliah yang to the point sesuai dengan program studi nya dan
-              disesuaikan dengan perkembangan industri
+      <div className=" bg-slate-300 p-7 mb-6 w-full box-border text-white">
+        <div class="flex justify-center ...">
+          <div className="w-[45%] rounded-md bg-blue-500/40 transition-all backdrop-blur-md hover:shadow-md hover:shadow-black hover:bg-blue-500/70 mr-2 hover:text-blue-900 p-5 box-border">
+            <p className="text-xl font-bold hover:underline mb-2">
+              Gedung Praktik
             </p>
-            <div className="text-left font-bold text-xl my-6">
-              <h1>Program Studi</h1>
-            </div>
-            <div className="bg-blue-500/40 px-9 rounded-lg py-5 mt-4 text-sm shadow-md shadow-black hover:shadow-yellow-900 hover:border-t-4 hover:border-yellow-800 transition-all hover:bg-gradient-to-bl hover:from-blue-400 hover:via-white/40 hover:to-yellow-400">
-              <h1 className="text-left text-xl underline ">
-                SARJANA TERAPAN TEKNIK INFORMATIKA
-              </h1>
-              <p className="text-justify p-2">
-                Program studi sarjana terapan teknik informatika merupakan
-                program studi yang berfokus pada pengembangan teknologi rekayasa
-                perngkat lunak. ranah dari prodi ini adalah berfokus pada
-                pengembangan seperti aplikasi, sistem operasi dan website maupun
-                data engineer. lulusan dari prodi teknik informatika PNJ
-                tersebar di berbagai sektor seperti Software Engineer, Website
-                Developer, maupun QA IT.
+            <p className="text-md text-left text-white mb-2">
+              Universitas Indonesia, Gedung AA PNJ, Jl. Prof.DR.G.A. Siwabessy,
+              Beji, Depok, Jawa Barat, 16525
+            </p>
+
+            <div className="ml-2">
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">Sekertariat HIMATIK</p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">
+                  Administrasi Prodi Teknik Komputer & Jaringan
+                </p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">
+                  Administrasi Prodi Teknik Multimedia Digital
+                </p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">Kantor Pimpinan TIK </p>
               </p>
             </div>
-            <div className="bg-blue-500/40 px-9 rounded-lg py-5 mt-4 text-sm shadow-md shadow-black hover:shadow-yellow-900 hover:border-t-4 hover:border-yellow-800 transition-all hover:bg-gradient-to-t hover:from-blue-400 hover:via-white/40 hover:to-yellow-400">
-              <h1 className="text-left text-xl underline">
-                SARJANA TERAPAN TEKNIK MULTIMEDIA JARINGAN{" "}
-              </h1>
-              <p className="text-justify p-2">
-                SARJANA TERAPAN TEKNIK MULTIMEDIA JARINGAN Program studi sarjana
-                terapan teknik multimedia jaringan berfokus pada pengembangan
-                infrastruktur jaringan atau interkonektivtas antar komputer.
-                ranah dari prodi ini terbagi menjadi beberapa peminatan seperti
-                embeded system, machine learning, cyber security, dan lain lain.
-                lulusan dari prodi ini tersebar diberbagai sektor seperti cyber
-                security, Telekomunikasi, teknisi sistem komputer,dsb
+            <div className="text-white text-left mt-2">
+              <span className="text-xl inline-block ">📞</span>
+              <p className="inline-block ml-4">+62 812 4567 8976</p>
+            </div>
+          </div>
+          <div className="w-[45%] rounded-md bg-blue-500/40 transition-all backdrop-blur-md hover:shadow-md hover:shadow-black hover:bg-blue-500/70 ml-2 hover:text-blue-900 p-5 box-border">
+            <p className="text-xl font-bold hover:underline mb-2">
+              Gedung Kuliah Bersama
+            </p>
+            <p className="text-md text-left text-white mb-2">
+              Universitas Indonesia, Gedung Serba Guna PNJ, Jl. Prof.DR.G.A.
+              Siwabessy, Beji, Depok, Jawa Barat, 16525
+            </p>
+
+            <div className="ml-2">
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">Ruang Sidang & Konsultasi</p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">
+                  Administrasi Prodi Teknik Informatika
+                </p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">
+                  Administrasi Prodi Teknik Multimedia Jaringan
+                </p>
+              </p>
+              <p className="text-sm text-left text-white">
+                <span className="text-xl inline-block text-black">🏛</span>
+                <p className="inline-block ml-4">Aula Serba Guna </p>
               </p>
             </div>
-            <div className="bg-blue-500/40 px-9 rounded-lg py-5 mt-4 text-sm shadow-md shadow-black hover:shadow-yellow-900 hover:border-t-4 hover:border-yellow-800 transition-all hover:bg-gradient-to-br hover:to-blue-400 hover:via-white/40 hover:from-yellow-400">
-              <h1 className="text-left text-xl underline">
-                SARJANA TERAPAN TEKNIK MULTIMEDIA DIGITAL{" "}
-              </h1>
-              <p className="text-justify p-2">
-                Program studi sarjana terapan teknik multimedia digital berfokus
-                pada pengembangan Multimedia seperti animasi, user interface,
-                user experience, game, sampai pada pengembangan teknologi
-                metaverse seperti avatar dsb. lulusan dari prodi ini tersebar di
-                berbagai sektor seperti web designer, game developer, animation
-                engineer, dsb.
-              </p>
-            </div>
-          </div>
-          <div className=" w-[30%] p-6 box-border ">
-            <div className="bg-slate-300 flex items-center w-full h-20 text-left p-4 rounded-xl border-l-4 border-r-4 border-blue-500 mb-4 font-bold">
-              Penerimaan Mahasiswa
-            </div>
-            <div className="bg-white flex items-center w-full h-20 text-left p-4 rounded-xl border-b-2  border-yellow-500 text-l  mb-3 shadow-md transition-all hover:shadow-black hover:underline">
-              <p>➡ SNBT</p>
-            </div>
-            <div className="bg-white flex items-center w-full h-20 text-left p-4 rounded-xl border-b-2 border-yellow-500 text-l  mb-3 shadow-md transition-all hover:shadow-black hover:underline">
-              <p>➡ SNBP</p>
-            </div>
-            <div className="bg-white flex items-center w-full h-20 text-left p-4 rounded-xl border-b-2 border-yellow-500 text-l  mb-3 shadow-md  transition-all hover:shadow-black hover:underline">
-              <p className=" ">➡ Simak PNJ</p>
-            </div>
-            <div className="bg-white flex items-center w-full h-20 text-left p-4 rounded-xl border-b-2 border-yellow-500 text-l  mb-3 shadow-md  transition-all hover:shadow-black hover:underline">
-              <p className=" ">➡ Jalur Kerja Sama</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="ml-24 mr-96 ">
-          <div className="text-left font-bold text-xl mt-10">
-            <h1>Capaian Lulusan</h1>
-          </div>
-
-          <div className="mt-4 text-md">
-            <p className="text-justify">
-              lulusan Teknik informatika dan Komputer untuk program studi
-              sarjana terapan, mereka dibekali sertifikasi profesi yang sesuai
-              dengan KKNI level IV yang setara dengan lulusan sarjana akademik.
-              tidak hanya itu, lulusan juga dibekali dengan sertifikat tes
-              bahasa inggris TOEIC yang menjadi syarat kelulusan.
-            </p>
-          </div>
-          <div className="mt-9 text-md">
-            <p className="text-justify">
-              masa studi sarjana terapan adalah setara dengan 144 SKS yang dapat
-              selsaikan dalam waktu 4 tahun atau selama lamanya adalah 5 tahun.
-              Mahasiswa lulusan TIK PNJ yang sah secara sidang yudisium berhak
-              menyadang gelar Sarjana terapan ilmu komputer atau S.Tr.Kom
-            </p>
-          </div>
-
-          <div className="text-left font-bold text-xl mt-10">
-            <h1>Kehidupan Mahasiswa</h1>
-          </div>
-
-          <div className="mt-4 text-md">
-            <p className="text-justify">
-              Selama berkuliah, mahasiswa tidak hanya dibekali pembelajaran
-              dalam kampus tetapi PNJ juga menyediakan wadah bagi mahasiswa
-              untuk bersosialisasi dan berorganisasi antar sesama mahasiswa.
-              mahasiswa TIK juga diberikan kesempatan untuk mengajar, magang,
-              dan studi independent melalui program MBKM yang dimulai pada
-              semester 5
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex">
-        <div className="my-20 rounded-md shadow-md shadow-black w-[60%] mx-auto ">
-          <div className="p-10">
-            <div>
-              <h1 className="text-xl font-bold text-blue-600 mb-4">
-                ORGANISASI
-              </h1>
-            </div>
-            <div className="flex justify-center items-center">
-              <div className="w-44 mx-5">
-                <img src={foto10} alt="" />
-              </div>
-              <div className="w-44 mx-5">
-                <img src={foto13} alt="" />
-              </div>
-              <div className="w-36 mx-5">
-                <img src={foto9} alt="" />
-              </div>
-            </div>
-            <div className="mt-10">
-              <h1 className="text-xl font-bold text-blue-600 mb-4">
-                KELOMPOK STUDI MAHASISWA
-              </h1>
-            </div>
-            <div className="flex justify-center items-center">
-              <div className="w-36 mx-5">
-                <img src={foto11} alt="" />
-              </div>
-              <div className="w-44 mx-5">
-                <img src={foto12} alt="" />
-              </div>
+            <div className="text-white text-left mt-2">
+              <span className="text-xl inline-block ">📞</span>
+              <p className="inline-block ml-4">+62 812 4567 8976</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="Footer text-white">
-        <div>
-          <div className="bg-zinc-800 pt-5 mx-auto ">
-            <div className="flex justify-between items-center mx-20 mb-10">
-              <div className="flex justify-between items-center">
-                <div className="ml-[-20px]">
-                  <img src={logo_pnj} alt="" className="w-28" />
-                </div>
-                <div className="mx-3">
-                  <h1 className="text-left text-l font-based">
-                    Departemen <br /> Teknik Informatika dan <br />
-                    Komputer
-                  </h1>
-                </div>
-              </div>
-
-              <div className="text-left">
-                <div>
-                  <h1 className="text-l font-based mb-2 border-b-2 border-white">
-                    Akademik
-                  </h1>
-                </div>
-                <ul className="font-based">
-                  <li className="hover:underline transition-all">
-                    Sarjana Terapan Teknik Informatika
-                  </li>
-                  <li>Sarjana Terapan Teknik Multimedia Digital</li>
-                  <li className="hover:underline transition-all">
-                    Sarjana Terapan Teknik Multimedia Digital dan Jaringan
-                  </li>
-                  <li className="hover:underline transition-all">
-                    D1 Teknik Komputer dan Jaringan
-                  </li>
-                </ul>
-              </div>
-
-              <div className="text-left">
-                <div className="text-l fon-based mb-2 border-b-2 border-white">
-                  Akses Cepat
-                </div>
-                <ul className="font-based">
-                  <li className="hover:underline transition-all">
-                    Web Utama PNJ
-                  </li>
-                  <li className="hover:underline transition-all">
-                    Siak Mahasiswa
-                  </li>
-                  <li className="hover:underline transition-all">E-Learning</li>
-                  <li className="hover:underline transition-all">
-                    Gudang Ilmu TIK
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              <hr className="text-slate-800" />
-              <p className="p-2">
-                Developed By Randy & Fajar, Calon Junior Web Devlopment 2022 -
-                2026
-              </p>
-            </div>
+      <div className="w-full text-center text-black box-border">
+        <div className="mx-auto text-blue-900 font-bold text-2xl mb-5">
+          Social Media Kami
+        </div>
+        <hr />
+        <div className="flex flex-row place-content-evenly mx-auto p-4 my-5">
+          <div className="rounded shadow-md border-l-4 border-blue-900 hover:border-l-0 hover:border-r-4 hover:shadow-black p-10 transition-all">
+            <span className="text-7xl text-blue-800">
+              <FontAwesomeIcon icon={faInstagram} />
+            </span>
+            <p className="my-5 font-bold">Instagram</p>
+            <p className="text-l  hover:text-blue-900">@jtik_pnj</p>
+          </div>
+          <div className="rounded shadow-md border-t-4 border-b-4 border-blue-900 hover:border-t-0 hover:border-b-0 hover:border-l-4 hover:border-r-4 hover:shadow-black p-10 transition-all">
+            <span className="text-7xl text-blue-800">
+              <FontAwesomeIcon icon={faTwitter} />
+            </span>
+            <p className="my-5 font-bold">Twitter</p>
+            <p className="text-l  hover:text-blue-900">@jtik_pnj</p>
+          </div>
+          <div className="rounded shadow-md border-r-4 border-blue-900 hover:border-r-0 hover:border-l-4 hover:shadow-black p-10 transition-all">
+            <span className="text-7xl text-blue-800">
+              <FontAwesomeIcon icon={faYoutube} />
+            </span>
+            <p className="my-5 font-bold">Youtube</p>
+            <p className="text-l hover:text-blue-900">jtik_pnj</p>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default academic;
+export default contact;
